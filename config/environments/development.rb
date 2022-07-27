d22bf53e0784b63a11f17a5688b26c948adb2647
yoshioka-chihiro <yoshokaaaa@gmail.com>
2022-07-27 07:43:26 +0000

    jQuery追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "cff4ceabc57e42f3882760fb1cb9ad1b.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
