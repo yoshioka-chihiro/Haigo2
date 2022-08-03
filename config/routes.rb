09f04a562efa7d4552ce200b2c7d335829c50679
yoshioka-chihiro <yoshokaaaa@gmail.com>
2022-08-03 07:31:13 +0000

    routes.rb基本設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   # 顧客用
   # URL /customers/sign_in …
   # パスワードの変更は不要だからスキップ
@@ -14,62 +14,27 @@ Rails.application.routes.draw do
   devise_for :admin,skip:[:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   root to: 'public/homes#top'
-  
-  scope module: public do
-    
+
+  scope module: :public do
+
     resources :recipes, only:[:index, :show, :edit, :create, :update, :destroy]
-    
+    resources :material_genres, only:[:new, :index, :edit]
+    resources :materials, only:[:index, :show]
+    resources :members, only:[:index, :show, :edit]
   end
 
-  namespace :public do
-    get 'recipes/index'
-    get 'recipes/show'
-    get 'recipes/new'
-    get 'recipes/edit'
-  end
   namespace :admin do
-    get 'material_genres/new'
-    get 'material_genres/index'
-    get 'material_genres/edit'
-  end
-  namespace :admin do
-    get 'allergies/index'
-    get 'allergies/edit'
-  end
-  namespace :admin do
-    get 'additives/index'
-    get 'additives/edit'
-  end
-  namespace :public do
-    get 'materials/index'
-    get 'materials/show'
-  end
-  namespace :admin do
-    get 'materials/index'
-    get 'materials/edit'
-    get 'materials/show'
-    get 'materials/new'
-  end
-  namespace :public do
-    get 'homes/top'
-  end
-  namespace :admin do
-    get 'homes/top'
+    get '/' => 'homes#top'
+    resources :material_genres, only:[:new, :index, :edit]
+    resources :allergies, only:[:index, :edit]
+    resources :additives, only:[:index, :edit]
+    resources :materials, only:[:index, :edit, :show, :new]
+    resources :members, only:[:index, :show, :edit]
   end
 
 
-  namespace :public do
-    get 'members/index'
-    get 'members/show'
-    get 'members/edit'
-  end
-  namespace :admin do
-    get 'members/index'
-    get 'members/show'
-    get 'members/edit'
-  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
