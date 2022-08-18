04825a74039d7b6db71502e9b4b98a07c27fbd66
yoshioka-chihiro <yoshokaaaa@gmail.com>
2022-08-18 14:29:18 +0000

    recipe index

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -2,4 +2,6 @@ class EndUser < ApplicationRecord
   has_many :ban_end_users, dependent: :destroy
   has_many :materials, through: :ban_end_users
   has_many :recipes, dependent: :destroy
+
+    enum name: {sb: 0, md: 1, seven: 2}
 end
