41682441b99e2f316e9083c3982a609536d1fc40
yoshioka-chihiro <yoshokaaaa@gmail.com>
2022-08-21 12:45:55 +0000

    recipe create

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -8,6 +8,24 @@ class Public::RecipesController < ApplicationController
   def new
   end
 
+  def create
+    @recipe = Recipe.new(recipes_params)
+    if @recipe.save
+      flash[:notice] = "登録完了"
+      redirect_to recipes_path
+    else
+      @recipes = Recipe.all
+      render "index"
+    end
+  end
+
   def edit
   end
+
+  private
+
+  def recipes_params
+    params.require(:recipe).permit(:name, :wholesale_id, :end_user_id, :manufacturing_cost, :for_use)
+  end
+
 end
