88a38ca935c82e9cbd5a0588328f9a523789456f
yoshioka-chihiro <yoshokaaaa@gmail.com>
2022-09-06 14:23:51 +0000

    recipe_material原料重複登録完成

--- app/controllers/public/recipe_materials_controller.rb
+++ app/controllers/public/recipe_materials_controller.rb
@@ -26,12 +26,35 @@ class Public::RecipeMaterialsController < ApplicationController
       render "new"
     end
   end
-  
+
+  def create
+    @material = Material.find(params[:recipe_material][:material_id])
+    @recipe = Recipe.find(params[:recipe_id])
+    if @recipe.recipe_materials.find_by(material_id: @material.id)
+      @new_recipe_material = RecipeMaterial.new(recipe_materials_params)
+      @recipe_material = @recipe.recipe_materials.find_by(material_id: @material.id)
+      @recipe_material.amount+= @new_recipe_material.amount
+      @recipe_material.save
+      flash[:notice] = "原料が追加されました"
+      redirect_to new_recipe_material_path(recipe_id: @recipe_material.recipe_id)
+    else
+      @recipe_material = RecipeMaterial.new(recipe_materials_params)
+      if @recipe_material.save!
+        flash[:notice] = "原料が新たに追加されました"
+        redirect_to new_recipe_material_path(recipe_id: @recipe_material.recipe_id)
+      else
+         @recipe_material = RecipeMaterial.new
+        flash[:notice] = "正しく入力してください"
+        render "new"
+      end
+    end
+  end
+
   def destroy
     @d_recipe_material = RecipeMaterial.find(params[:id])
     if @d_recipe_material.destroy
       flash[:notice] = "削除しました"
-      
+
       redirect_to new_recipe_material_path(recipe_id: @d_recipe_material.recipe_id)
     else
       @recipe_material = RecipeMaterial.new
--- app/models/recipe_material.rb
+++ app/models/recipe_material.rb
@@ -2,22 +2,5 @@ class RecipeMaterial < ApplicationRecord
   belongs_to :material
   belongs_to :recipe
   
-  # タグ保存
-  # def save_tags(tags)
-  # current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
-  # old_tags = current_tags - tags
-  # new_tags = tags - current_tags
-
-  # # Destroy
-  # old_tags.each do |old_name|
-  #   self.tags.delete Tag.find_by(tag_name:old_name)
-  # end
-
-  # # Create
-  # new_tags.each do |new_name|
-  #   diary_tag = Tag.find_or_create_by(tag_name:new_name)
-  #   self.tags << diary_tag
-  # end
-  # end
   
 end
